67108b3f43aca1fc59fc6df2a46dcd1be180618f
Samiksha Barnwal <120215885+wildtigress@users.noreply.github.com>
2026-01-28 17:25:56 +0530

    Add CORS middleware to server

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
+app.use(cors());
 
 const app = express();
 app.use(cors());
